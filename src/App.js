f7989935d43bb8f168c4d968db1b2757f87cfb44
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-03-09 18:56:49 -0300

    Cria estrutura inicial do projeto.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import firebase from "./services/firebaseConnection";
+
+function App() {
+  return (
+    <div>
+      <h1>Teste</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import 'firebase/auth';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyAoZRT4INpROOFrHASenBAB6NYFkKN0yZY",
+    authDomain: "sistema-chamados-beef3.firebaseapp.com",
+    projectId: "sistema-chamados-beef3",
+    storageBucket: "sistema-chamados-beef3.appspot.com",
+    messagingSenderId: "397609083904",
+    appId: "1:397609083904:web:45943606d64674b28624e5",
+    measurementId: "G-G1691PXZ00"
+  };
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
