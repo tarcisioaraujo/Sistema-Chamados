5b4a647bd5ceb3b13570ca0c4fc14ccacf08b2d1
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-03-09 19:51:36 -0300

    Cria formulário de criação de conta.

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,36 @@
 
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logo.png';
+
 function SignUp() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('CLICOU')
+  }
+
   return (
-    <div>
-      <h1>TELA DE CADASTRO</h1>
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Sistema Logo" />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Criar uma conta</h1>
+          <input type="text" placeholder="Seu nome" value={nome} onChange={(e) => setNome(e.target.value)} />
+          <input type="text" placeholder="email@email.com" value={email} onChange={ (e) => setEmail(e.target.value) }/>
+          <input type="password" placeholder="*******" value={password} onChange={(e) => setPassword(e.target.value) } />
+          <button type="submit">Cadastrar</button>
+        </form>  
+
+        <Link to="/">Já tem uma conta? Entre</Link>
+      </div>
     </div>
   );
 }
