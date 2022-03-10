d256cc916b167a0ce2dd0ea30ad2b7dac4420d5d
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-03-10 19:35:31 -0300

    Desenvolve mecanismo de login.

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -26,6 +26,36 @@ function AuthProvider({ children }){
 
   }, [])
 
+  async function signIn(email, password){
+    setLoadingAuth(true);
+
+    await firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(async (value)=> {
+      let uid = value.user.uid;
+
+      const userProfile = await firebase.firestore().collection('users')
+      .doc(uid).get();
+
+      let data = {
+        uid: uid,
+        nome: userProfile.data().nome,
+        avatarUrl: userProfile.data().avatarUrl,
+        email: value.user.email
+      };
+
+      setUser(data);
+      storageUser(data);
+      setLoadingAuth(false);
+
+
+    })
+    .catch((error)=>{
+      console.log(error);
+      setLoadingAuth(false);
+    })
+
+  }
+
   async function signUp(email, password, nome){
     setLoadingAuth(true);
 
@@ -79,7 +109,8 @@ function AuthProvider({ children }){
       user,
       loading,
       signUp,
-      signOut
+      signOut,
+      signIn
     }}>
       {children}
     </AuthContext.Provider>
