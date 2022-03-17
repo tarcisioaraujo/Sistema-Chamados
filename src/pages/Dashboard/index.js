b7dba4d5ce87bb2f5f8497fc7736205c45ba3b4d
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-03-17 19:31:44 -0300

    Constroi tela de chamados com botão de adicionar novo chamado estilizado.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,13 @@
-import { useContext } from 'react';
-import { AuthContext } from '../../contexts/auth';
+import './dashboard.css';
+import { useState } from 'react';
 
 import Header from '../../components/Header';
 import Title from '../../components/Title';
-import { FiMessageSquare } from 'react-icons/fi';
+import { FiMessageSquare, FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
-export default function Dashboard(){
-  const { signOut } = useContext(AuthContext);
+export default function Dashboard() {
+  const [chamados, setChamados] = useState([]);
 
   return(
     <div>
@@ -16,8 +17,26 @@ export default function Dashboard(){
         <Title name="Atendimentos">
           <FiMessageSquare size={25} />
         </Title>
+
+        {chamados.length === 0 ? (
+          <div className="container dashboard">
+            <span>Nenhum chamado registrado...</span>
+
+            <Link to="/new" className="new">
+              <FiPlus size={25} color="#FFF" />
+              Novo chamado
+            </Link>
+          </div>
+        ) : (
+          <>
+            <Link to="/new" className="new">
+              <FiPlus size={25} color="#FFF" />
+              Novo chamado
+            </Link>
+          </>
+        )}  
+
       </div>
-      
     </div>
   )
 }
