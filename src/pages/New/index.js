b67aa5f531d90226a35fa912aff55cbfb199e693
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-03-17 20:29:00 -0300

    Constroi formulário de cadastro de chamados com algumas opções ainda estáticas.

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -6,6 +6,12 @@ import './new.css';
 import { FiPlusCircle } from 'react-icons/fi'
 
 export default function New() {
+
+  function handleRegister(e){
+    e.preventDefault();
+    console.log('submit handleRegister');
+  }
+
   return(
     <div>
       <Header/>
@@ -14,6 +20,58 @@ export default function New() {
         <Title name="Novo chamado">
           <FiPlusCircle size={25} />
         </Title>
+
+        <div className="container">
+
+          <form className="form-profile"  onSubmit={handleRegister} >
+            
+            <label>Cliente</label>
+            <select>
+              <option key={1} value={1}>
+                Empresa ABC
+              </option>
+            </select>
+
+            <label>Assunto</label>
+            <select>
+              <option value="Suporte">Suporte</option>
+              <option value="Visita Tecnica">Visita Tecnica</option>
+              <option value="Financeiro">Financeiro</option>
+            </select>
+
+            <label>Status</label>
+            <div className="status">
+              <input 
+                type="radio"
+                name="radio"
+                value="Aberto"
+              />
+              <span>Em Aberto</span>
+
+              <input 
+                type="radio"
+                name="radio"
+                value="Progresso"
+              />
+              <span>Progresso</span>
+
+              <input 
+                type="radio"
+                name="radio"
+                value="Atendido"
+              />
+              <span>Atendido</span>
+            </div>
+
+            <label>Complemento</label>
+            <textarea
+              type="text"
+              placeholder="Descreva seu problema (opcional)."
+            />
+            
+            <button type="submit">Registrar</button>
+          </form>
+        </div>        
       </div>
     </div>
   )
