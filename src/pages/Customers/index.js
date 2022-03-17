bf54df3a4997863ccc5ef83ae858b0290b0db555
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-03-17 19:00:02 -0300

    Constroi formulário de cadastro de cliente.

--- src/pages/Customers/index.js
+++ src/pages/Customers/index.js
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import './customers.css';
 import Title from '../../components/Title';
 import Header from '../../components/Header';
@@ -6,6 +7,16 @@ import Header from '../../components/Header';
 import { FiUser } from 'react-icons/fi';
 
 export default function Customers(){
+  const [nomeFantasia, setNomeFantasia] = useState('');
+  const [cnpj, setCnpj] = useState('');
+  const [endereco, setEndereco] = useState('');
+
+
+
+  function handleAdd(e){
+    e.preventDefault();
+    console.log('submit do formulário');
+  }
 
   return(
     <div>
@@ -15,6 +26,23 @@ export default function Customers(){
       <Title name="Clientes">
         <FiUser size={25} />
       </Title>
+
+      <div className="container">
+        <form className="form-profile customers" onSubmit={handleAdd}>
+          <label>Nome fantasia</label>
+          <input type="text" placeholder="Nome da sua empresa" value={nomeFantasia} onChange={ (e) => setNomeFantasia(e.target.value) } />
+
+          <label>CNPJ</label>
+          <input type="text" placeholder="Seu CNPJ" value={cnpj} onChange={ (e) => setCnpj(e.target.value) } />
+
+          <label>Endereço</label>
+          <input type="text" placeholder="Endereço da empresa" value={endereco} onChange={ (e) => setEndereco(e.target.value) } />
+
+          <button type="submit">Cadastrar</button>
+
+        </form>
+      </div>
+
     </div>
 
     </div>
