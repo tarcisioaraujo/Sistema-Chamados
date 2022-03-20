a4f02837ee4a1f2a27c6caebf7c951aa39eda9cf
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-03-20 20:41:50 -0300

    Desenvolve loading enquanto obtém chamads do Firestore.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -70,6 +70,25 @@ export default function Dashboard() {
     setLoadingMore(false);
   }
 
+  if(loading){
+    return(
+      <div>
+        <Header/>
+
+        <div className="content">
+          <Title name="Atendimentos">
+            <FiMessageSquare size={25} />
+          </Title>  
+
+          <div className="container dashboard">
+            <span>Buscando chamados...</span>
+          </div>
+
+        </div>      
+      </div>
+    )
+  }
+
   return(
     <div>
       <Header/>
