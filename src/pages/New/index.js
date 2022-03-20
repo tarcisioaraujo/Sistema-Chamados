ac13b1a26c29093fe856833a9cb74e2b0c2613fa
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-03-20 18:57:32 -0300

    Persiste no Firestore registro de chamado.

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -6,6 +6,7 @@ import firebase from '../../services/firebaseConnection';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
 import { AuthContext } from '../../contexts/auth';
+import { toast } from 'react-toastify';
 
 import './new.css';
 import { FiPlusCircle } from 'react-icons/fi'
@@ -58,9 +59,28 @@ export default function New() {
 
   }, []);
 
-  function handleRegister(e){
+  async function handleRegister(e){
     e.preventDefault();
-    console.log('submit handleRegister');
+    
+    await firebase.firestore().collection('chamados')
+    .add({
+      created: new Date(),
+      cliente: customers[customerSelected].nomeFantasia,
+      clienteId: customers[customerSelected].id,
+      assunto: assunto,
+      status: status,
+      complemento: complemento,
+      userId: user.uid
+    })
+    .then(()=> {
+      toast.success('Chamado criado com sucesso!');
+      setComplemento('');
+      setCustomerSelected(0);
+    })
+    .catch((err)=> {
+      toast.error('Ops erro ao registrar chamado, tente mais tarde.');
+      console.log(err);
+    })
   }
 
   // Quando troca o assunto
@@ -96,7 +116,7 @@ export default function New() {
             {loadCustomers ? (
               <input type="text" disabled={true} value="Carregando clientes..." />
             ) : (
-                <select value={customerSelected} onChange={handleChangeCustomers} >
+              <select value={customerSelected} onChange={handleChangeCustomers} >
                 {customers.map((item, index) => {
                   return(
                     <option key={item.id} value={index} >
